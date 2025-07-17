3f3e7e86c151e099251fe0eb2a8b7d66662c1802
jatinkumar022 <jatinrmn@gmail.com>
2025-07-17 16:03:35 +0530

    fixed another bug

--- src/app/(main)/profile/page.tsx
+++ src/app/(main)/profile/page.tsx
@@ -184,7 +184,10 @@ export default function ProfilePage() {
                         value={label}
                         className="border-b overflow-hidden"
                       >
-                        <AccordionTrigger className="px-3 py-3 rounded-none text-left hover:bg-zinc-100 data-[state=open]:text-blue-600 data-[state=open]:border-l-2 data-[state=open]:border-blue-600 border-l-2 border-transparent hover:border-blue-600">
+                        <AccordionTrigger
+                          className="px-3 py-3 rounded-none text-left hover:bg-zinc-100 dark:hover:bg-[#1e1e1e]
+            data-[state=open]:text-blue-600 data-[state=open]:border-l-2 data-[state=open]:border-blue-600 border-l-2 border-transparent hover:border-blue-600"
+                        >
                           {label}
                         </AccordionTrigger>
                         <AccordionContent className="px-2 pt-2 pb-2 border-t">
@@ -197,9 +200,9 @@ export default function ProfilePage() {
                                     onClick={() => setSelectedPage(item.slug)}
                                     className={`block text-start cursor-pointer px-3 py-2 w-full text-zinc-700 hover:text-blue-600 border-l-2 hover:border-blue-600 ${
                                       isActive
-                                        ? "text-blue-600 bg-zinc-100 border-blue-600 font-medium"
-                                        : "text-zinc-700 border-transparent hover:text-blue-600 hover:bg-zinc-100 hover:border-blue-600"
-                                    }`}
+                                        ? "text-blue-600 bg-zinc-100 dark:bg-[#1e1e1e] border-blue-600 font-medium"
+                                        : "text-zinc-700 border-transparent hover:text-blue-600 hover:bg-zinc-100 dark:hover:bg-[#1e1e1e] hover:border-blue-600"
+                                    } `}
                                   >
                                     {item.label}
                                   </button>
@@ -217,7 +220,7 @@ export default function ProfilePage() {
                             label.toLowerCase().replace(/\s+/g, "-")
                           )
                         }
-                        className="block px-3 py-3 w-full text-left hover:bg-zinc-100 text-zinc-700 border-l-transparent border-l-2 border-b hover:border-l-blue-600"
+                        className="block px-3 py-3  w-full text-left  hover:bg-zinc-100 dark:hover:bg-[#1e1e1e] cursor-pointer text-zinc-700 border-l-transparent   border-l-2 border-b hover:border-l-blue-600"
                       >
                         {label}
                       </button>
